3aa271c827f9af5fdbce69121d302bb2dd44be54
zacharyinserra <zachary@christiansteven.com>
2020-03-18 18:12:59 -0400

    committed to relationship

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "1e8f1efdb222a5a4c11c6e9d2cdd2287"
+  const unit = "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&&units=" + unit;
+
+  https.get(url, function(response) {
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDesc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>Weather is " + weatherDesc + "</p>");
+      res.write("<h1>Temperature in " + query + " is " + temp + " C</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server running on port 3000...");
+});
