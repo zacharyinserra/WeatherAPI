094cc6b33c27167535d3d7e480d372f10bd7507a
zacharyinserra <zacharyinserra@gmail.com>
2020-08-03 18:55:39 -0400

    try/catch

--- app.js
+++ app.js
@@ -22,15 +22,19 @@ app.post("/", function(req, res) {
   https.get(url, function(response) {
 
     response.on("data", function(data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDesc = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write("<p>Weather is " + weatherDesc + "</p>");
-      res.write("<h1>Temperature in " + query + " is " + temp + " C</h1>");
-      res.write("<img src=" + imageURL + ">");
-      res.send();
+      try {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDesc = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write("<p>Weather is " + weatherDesc + "</p>");
+        res.write("<h1>Temperature in " + query + " is " + temp + " C</h1>");
+        res.write("<img src=" + imageURL + ">");
+        res.send();
+      } catch(err) {
+        console.log(err);
+      }
     });
   });
 });
